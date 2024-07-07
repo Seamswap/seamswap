9de56c66e5c30d87648f82021b024ed3257007a5
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-07-07 16:20:05 +0100

    improvements: added new theme

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import { cn } from '@src/lib/utils';
 
 
-export interface IInputProps {
+export interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+
 }
 
-const Input: React.FC<IInputProps> = props => {
-  return <input placeholder="0.0" className="border-primary pl-4 text-2xl w-1/2 rounded-xl py-2" />;
+const Input: React.FC<IInputProps> = ({ className, placeholder, ...props }) => {
+  return <input {...props} placeholder={placeholder || '0.0'}
+                className={cn('border-primary pl-4 rounded-xl py-2 flex w-full rounded-md border bg-white text-lg ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', className)} />;
 };
 export default Input;
