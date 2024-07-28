70781857dc6d48b8003ef941108fdbaeb8cb4ed7
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-07-28 16:39:01 +0100

    chore: ignored type error

--- src/lib/shared/useSeamlessContractRead.ts
+++ src/lib/shared/useSeamlessContractRead.ts
@@ -45,6 +45,7 @@ export function useSeamlessContractRead<
   // ************* //
   // Read contract //
   // ************* //
+  // @ts-ignore
   const result = useReadContract({ ...parameters });
 
   return {
